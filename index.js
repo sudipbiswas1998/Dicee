a14e1d83fee8b5de45bd5c89a79a8741fc286ca3
Sudip Biswas <47990013+sudipbiswas1998@users.noreply.github.com>
2022-07-27 10:17:29 +0530

    index.js update

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ console.log(randomNumber2);
 document.querySelector(".img2").setAttribute("src", "images/dice" + randomNumber2 + ".png");
 if (randomNumber1 > randomNumber2) {
   document.querySelector("h1").innerHTML = "Player 1 Wins! 🚩";
-}else{
+}else if(randomNumber1 < randomNumber2){
     document.querySelector("h1").innerHTML = "Player 2 Wins! 🚩";
+}else if(randomNumber1 === randomNumber2){
+    document.querySelector("h1").innerHTML = "Draw! 🚩";
 }
